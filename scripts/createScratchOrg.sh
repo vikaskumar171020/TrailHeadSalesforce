dece5648aa11b555804803fa0e01ac8632de53ca
Vikaskumar171020@gmail.com <Vikaskumar@cyntexa.com>
2022-07-08 01:06:29 +0530

    All Set

--- scripts/createScratchOrg.sh
+++ scripts/createScratchOrg.sh
@@ -1,16 +1,39 @@
-# this is use to create a new scratchorg and delete old scratch org
-# !bin/bash
-DEV_HUB_ALIAS="vikaskumar171020@wise-unicorn-51gmzk.com"
-SCRATCH_ORG_ALIAS="TrailHeadOrg"
+#!/bin/bash
 
-echo "set default dev hub user:" $DEV_HUB_ALIAS
+DEV_HUB_ALIAS="vikaskumar171020@wise-unicorn-51gmzk.com"    #your username
+SCRATCH_ORG_ALIAS="TrailHeadOrg"               #your scratch org name
+# PACKAGENAME="stripe cyntexa"                    #package name
+
+
+echo "set default devhub user :" $DEV_HUB_ALIAS
 sfdx force:config:set defaultdevhubusername=$DEV_HUB_ALIAS
 
-echo "deleting old scratch org:" $SCRATCH_ORG_ALIAS
-sfdx force:org:delete -p -u $scratch_org_alias
+echo "deleting old scratch org :" $SCRATCH_ORG_ALIAS
+sfdx force:org:delete -p -u $SCRATCH_ORG_ALIAS
+rm -rf ./.sfdx/orgs
 
-echo "creating new scratch org..."
-sfdx force:org:create -a $scratch_org_alias -s -f ./config/project-scratch-def.json -d 30
+echo "Creating scratch ORG..."
+sfdx force:org:create -a $SCRATCH_ORG_ALIAS -s -f ./config/project-scratch-def.json -d 30
 
-echo "pushing changes to scratch org..."
+echo "Pushing changes to scratch org..."
 sfdx force:source:push
+
+# echo "Assigning Admin permission set to running user."
+# sfdx force:user:permset:assign --permsetname ChargeOn_Admin --targetusername $SCRATCH_ORG_ALIAS
+
+# echo "Assigning permission"
+# execute sfdx force:user:permset:assign -n {Permission Set}
+
+# echo "Make sure Org user is english"
+# sfdx force:data:record:update -s User -w "Name='User User'" -v "Languagelocalekey=en_US"
+
+# echo "Running apex tests..."
+# execute sfdx force:apex:test:run -l RunLocalTests -w 30
+
+
+# if [ -f "package.json" ]; then
+#   echo "Running jest tests"
+#   execute npm install
+#   execute npm run test:unit
+# fi
+
