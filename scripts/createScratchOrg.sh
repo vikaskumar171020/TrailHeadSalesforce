d68b2dbc761449b6bd27e023b3d75cecefe9b535
Vikaskumar171020@gmail.com <Vikaskumar@cyntexa.com>
2022-07-08 01:07:29 +0530

    All Set

--- scripts/createScratchOrg.sh
+++ scripts/createScratchOrg.sh
@@ -2,7 +2,6 @@
 
 DEV_HUB_ALIAS="vikaskumar171020@wise-unicorn-51gmzk.com"    #your username
 SCRATCH_ORG_ALIAS="TrailHeadOrg"               #your scratch org name
-# PACKAGENAME="stripe cyntexa"                    #package name
 
 
 echo "set default devhub user :" $DEV_HUB_ALIAS
@@ -17,23 +16,3 @@ sfdx force:org:create -a $SCRATCH_ORG_ALIAS -s -f ./config/project-scratch-def.j
 
 echo "Pushing changes to scratch org..."
 sfdx force:source:push
-
-# echo "Assigning Admin permission set to running user."
-# sfdx force:user:permset:assign --permsetname ChargeOn_Admin --targetusername $SCRATCH_ORG_ALIAS
-
-# echo "Assigning permission"
-# execute sfdx force:user:permset:assign -n {Permission Set}
-
-# echo "Make sure Org user is english"
-# sfdx force:data:record:update -s User -w "Name='User User'" -v "Languagelocalekey=en_US"
-
-# echo "Running apex tests..."
-# execute sfdx force:apex:test:run -l RunLocalTests -w 30
-
-
-# if [ -f "package.json" ]; then
-#   echo "Running jest tests"
-#   execute npm install
-#   execute npm run test:unit
-# fi
-
