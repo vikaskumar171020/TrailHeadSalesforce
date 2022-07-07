9dbfb0485c96779051446101cd3b809371d62ddc
Vikaskumar171020@gmail.com <Vikaskumar@cyntexa.com>
2022-07-08 01:17:51 +0530

    Update

---
+++ scripts/createScratchOrg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DEV_HUB_ALIAS="vikaskumar171020@wise-unicorn-51gmzk.com"    #your username
+SCRATCH_ORG_ALIAS="TrailHeadOrg"               #your scratch org name
+
+
+echo "set default devhub user :" $DEV_HUB_ALIAS
+sfdx force:config:set defaultdevhubusername=$DEV_HUB_ALIAS
+
+echo "deleting old scratch org :" $SCRATCH_ORG_ALIAS
+sfdx force:org:delete -p -u $SCRATCH_ORG_ALIAS
+rm -rf ./.sfdx/orgs
+
+echo "Creating scratch ORG..."
+sfdx force:org:create -a $SCRATCH_ORG_ALIAS -s -f ./config/project-scratch-def.json -d 30
+
+echo "Pushing changes to scratch org..."
+sfdx force:source:push
+# to update more
